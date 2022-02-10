6200b067a9a53f3608b01a02592622fd1a0cadfa
Matthias Diener <mdiener@illinois.edu>
2022-02-10 13:02:45 -0600

    cleanups

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-OS=$(uname) | tr '[:upper:]' '[:lower:]'
-ARCH=$(uname -m) | tr '[:upper:]' '[:lower:]'
+MYOS=$(uname) | tr '[:upper:]' '[:lower:]'
+MYARCH=$(uname -m) | tr '[:upper:]' '[:lower:]'
 
-./build LIBS netlrts-$OS-$ARCH smp --with-production -g -j${CPU_COUNT}
+./build LIBS netlrts-$MYOS-$MYARCH smp --with-production -g -j${CPU_COUNT}
 
 
