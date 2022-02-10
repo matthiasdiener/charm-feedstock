06a3ffe321779263985e51fba4b1f4b83da465f5
Matthias Diener <mdiener@illinois.edu>
2022-02-10 13:55:28 -0600

    fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-MYOS=$(uname) | tr '[:upper:]' '[:lower:]'
-MYARCH=$(uname -m) | tr '[:upper:]' '[:lower:]'
+MYOS=$(uname | tr '[:upper:]' '[:lower:]')
+MYARCH=$(uname -m)
 
 ./build LIBS netlrts-$MYOS-$MYARCH smp --with-production -g -j${CPU_COUNT}
 
